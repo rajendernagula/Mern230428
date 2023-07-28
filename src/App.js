a1184e7a95ba8dfc45d7acc3328199c236f70236
N RAJENDER <rajendernagula206@gmail.com>
2023-07-28 12:02:01 +0530

    added git repository my myself

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Iam Rajender repository create by myself</h2>
+        <h1>Git Hub</h1>
+        <p>
+          GitHub is a code hosting platform for version control and
+          collaboration. It lets you and others work together on projects from
+          anywhere. This tutorial teaches you GitHub essentials like
+          repositories, branches, commits, and pull requests.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
